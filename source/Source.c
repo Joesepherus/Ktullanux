c9fb897414ba8dcafe9e235cf478b9c0a9ebfcf1
Jozef Maloch <Jozefmaloch@gmail.com>
2017-09-28 23:09:55 +0200

    5. almost working, added a whole new way of doing things

--- source/Source.c
+++ source/Source.c
@@ -110,8 +110,209 @@ void hexDump(void *addr, int len) {
 	printf("  %s\n", buff);
 }
 
-void ipDump(void *addr, int len) {
+
+// ================================================================================================================================================================
+#define GET_SIZE(p) ((sizeof(p)/sizeof(p[0]))
+
+typedef struct icmp {
+	int icmp_type[2];
+} ICMP;
+
+typedef struct tcp {
+	int source_port[2];
+	int dest_port[2];
+} TCP;
+
+typedef struct ip {
+	int source_ip[4];
+	int dest_ip[4];
+	int protocol_ip;
+	char ip_type[2];
+	struct tcp next;
+	//struct arp arp;
+	struct icmp icmp;
+} IP;
+
+typedef struct ether {
+	unsigned short id;
+	unsigned short frame_length_pcap;
+	unsigned short frame_length_cable;
+	int source_MA[6];
+	int dest_MA[6];
+	char ether_type[4];
+	struct ip next;
+	int hexDump[250];
+} ETHER;
+
+typedef struct arp {
+	int operation[4];
+	int arp_req_ip[8];
+	int arp_reply_ip[8];
+} ARP;
+
+void printHexa(int *ptr, int size) {
+	for (int i = 0; i < size; i++) {
+		if (ptr[i] == '9999') return;
+		printf("%02x ", ptr[i]);
+	}
+	putchar('\n');
+}
+
+void printEther(struct ether ethernet) {
+	printf("id=%d\nlength=%d\nlength2=%d\n", ethernet.id, ethernet.frame_length_pcap, ethernet.frame_length_cable);
+	// TO DO vypis typu ramca(ethernet or llc)
+	printHexa(ethernet.source_MA, GET_SIZE(ethernet.source_MA)));
+	printHexa(ethernet.dest_MA, GET_SIZE(ethernet.dest_MA)));
+	printf("%s\n", ethernet.ether_type);
+
+	if (strcmp(ethernet.ether_type, "IP") == 0) {
+		printHexa(ethernet.next.dest_ip, GET_SIZE(ethernet.next.dest_ip)));
+		printHexa(ethernet.next.source_ip, GET_SIZE(ethernet.next.source_ip)));
+	}
+
+	printf("%s\n", ethernet.next.ip_type);
+
+	if (strcmp(ethernet.next.ip_type, "TCP") == 0) {
+		printHexa(ethernet.next.next.dest_port, GET_SIZE(ethernet.next.next.dest_port)));
+		printHexa(ethernet.next.next.source_port, GET_SIZE(ethernet.next.next.source_port)));
+	}
+
+	if (strcmp(ethernet.next.ip_type, "ICMP") == 0) {
+		printHexa(ethernet.next.icmp.icmp_type, GET_SIZE(ethernet.next.icmp.icmp_type)));
+	}
+
+	// TO DO
+	if (strcmp(ethernet.next.ip_type, "ARP") == 0) {
+
+	}
+}
+
+void printAllEthers(struct ether *all, int size) {
 	int i;
+	for (i = 0; i < size; i++) {
+		printEther(all[i]);
+	}
+}
+
+struct ether ether_dump(void *addr, int len, struct ether ethernet) {
+	int i;
+	unsigned char *pc = (unsigned char*)addr;
+
+	if (len <= 0) {
+		printf("frames length can't be lower than 1");
+	}
+
+	// Process every byte in the data.
+	for (i = 0; i < 14; i++) {
+		if (i >= 0 && i < 6) {
+			ethernet.dest_MA[i] = pc[i];
+		}
+		else if (i >= 6 && i < 12)
+			ethernet.source_MA[i - 6] = pc[i];
+		else if (i >= 12 && i < 14)
+			if (pc[12] == 8 && pc[13] == 0) {
+				strcpy(ethernet.ether_type, "IP");
+			}
+		ethernet.hexDump[i] = pc[i];
+	}
+	return ethernet;
+}
+
+struct ether ip_dump(void *addr, struct ether ethernet) {
+	int i;
+	unsigned char *pc = (unsigned char*)addr;
+	// ip protocol
+
+	for (i = 14; i < 34; i++) {
+		if (i == 23) {
+			if (pc[i] == 6) {
+				strcpy(ethernet.next.ip_type, "TCP");
+			}
+			if (pc[i] == 1) {
+				strcpy(ethernet.next.ip_type, "ICMP");
+			}
+		}
+
+		// ip addresses
+		if (i >= 26 && i < 30)
+			ethernet.next.source_ip[i - 26] = pc[i];
+		if (i >= 30 && i < 34)
+			ethernet.next.dest_ip[i - 30] = pc[i];
+
+		ethernet.hexDump[i] = pc[i];
+		ethernet.hexDump[34] = '9999';
+	}
+	return ethernet;
+}
+
+struct tcp tcp_dump(void *addr) {
+	int i;
+	unsigned char *p = (unsigned char*)addr;
+	struct tcp tcp;
+
+	// tcp protocol port
+	for (i = 34; i < 38; i++) {
+		if (i >= 34 && i < 36)
+			tcp.source_port[i - 34] = p[i];
+
+		else if (i >= 36 && i < 38)
+			tcp.dest_port[i - 36] = p[i];
+
+	}
+	return tcp;
+}
+
+struct icmp icmp_dump(void *addr) {
+	int i;
+	unsigned char *p = (unsigned char*)addr;
+	struct icmp icmp;
+
+	for (i = 34; i < 36; i++) {
+		// icpm protocol port
+		if (i >= 34 && 36) {
+			icmp.icmp_type[i - 34] = p[i];
+		}
+	}
+
+	return icmp;
+}
+
+
+struct ether *dump(void *addr, struct pcap_pkthdr packet_header, int id) {
+	int i;
+	unsigned char *pc = (unsigned char*)addr;
+	struct ether ethernet;
+	struct ip ip;
+	struct tcp tcp;
+	struct icmp icmp;
+	struct ether all_eths[50];
+
+	ethernet.id = id;
+	ethernet.frame_length_pcap = packet_header.len;
+	ethernet.frame_length_cable = packet_header.caplen;
+
+	ethernet = ether_dump(pc, packet_header.len, ethernet);
+	if (strcmp(ethernet.ether_type, "IP") == 0) {
+		ethernet = ip_dump(pc, ethernet);
+	}
+	if (strcmp(ethernet.next.ip_type, "TCP") == 0) {
+		tcp = tcp_dump(pc);
+		ethernet.next.next = tcp;
+	}
+	if (strcmp(ethernet.next.ip_type, "ICMP") == 0) {
+		icmp = icmp_dump(pc);
+		ethernet.next.icmp = icmp;
+	}
+	printEther(ethernet);
+	printf("\nhexdump\n");
+	printHexa(ethernet.hexDump, GET_SIZE(ethernet.hexDump)));
+	all_eths[id] = ethernet;
+	putchar('\n');
+	return all_eths;
+}
+
+void ipDump(void *addr, int len) {
+	int i, ip_protocol;
 	unsigned char buff[17];
 	unsigned char *pc = (unsigned char*)addr;
 
@@ -126,41 +327,16 @@ void ipDump(void *addr, int len) {
 		return;
 	}
 
-	// Process every byte in the data.
-	for (i = 0; i < len; i++) {
-		// Multiple of 16 means new line (with line offset).
+	
 
-		/*if ((i % 16) == 0) {
-			// Just don't print ASCII for the zeroth line.
-			if (i != 0)
-				//printf("  %s\n", buff);
 
-			// Output the offset.
-			//printf("  %04x \n", i);
-		}*/
 
-		/*if (i % 8 == 0) {
-			printf(" ");
-		}*/
-		if (i >= 12 && i < 14) {
-			// Now the hex code for the specific character.
-			if (i != 13) {
-				printf("\n");
-			}
-			else {
-				printf(" ");
-			}
-			printf("%02x", pc[i]);
-		}
-		if (i == 14) {
-			printf("\n");
-		}
-		if (i >= 0 && i < 12 ) {
-			if (i == 8) {
-				printf("\n");
-			}
-			printf("%02x ", pc[i]);
-		}
+
+
+
+
+
+
 		// And store a printable ASCII character for later.
 		if ((pc[i] < 0x20) || (pc[i] > 0x7e))
 			buff[i % 16] = '.';
@@ -177,7 +353,7 @@ void ipDump(void *addr, int len) {
 
 	// And print the final ASCII bit.
 	//printf("  %s\n", buff);
-}
+
 
 void dumpHex(void* data, size_t size) {
 	char ascii[17];
@@ -213,7 +389,7 @@ void dumpHex(void* data, size_t size) {
 int packet_number = 1;
 
 void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {
-	printf("Packet Number: %d\n", packet_number++);
+	//printf("Packet Number: %d\n", packet_number++);
 	printf("Packet capture length: %d\n", packet_header.caplen);
 	printf("Packet total length %d\n", packet_header.len);
 	printf("\n");
@@ -221,18 +397,18 @@ void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header) {
 
 void packet_handler(
 	u_char *args,
-		const struct pcap_pkthdr *packet_header,
-		const u_char *packet_body
+	const struct pcap_pkthdr *packet_header,
+	const u_char *packet_body,
+	int counter
 	)
 {
-	ipDump(packet_body, packet_header->len);
-	print_packet_info(packet_body, *packet_header);
+
+	/*ipDump(packet_body, packet_header);
+	print_packet_info(packet_body, *packet_header);*/
+	dump(packet_body, *packet_header, counter++);
+
 	return;
 }
-
-
-
-
 
 int main(int argc, char *argv[]) {
 	pcap_t *pcap;
@@ -263,17 +439,24 @@ int main(int argc, char *argv[]) {
 	// hexDump(pcap, sizeof(pcap) + 7500);
 	// printf("%d\n==========================================\n", sizeof(pcap));
 
-	//while ((packet = pcap_next(pcap, &header)) != NULL && counter <= 10) {	
+	while ((packet = pcap_next(pcap, &header)) != NULL) {	
 		//dump_UDP_packet(packet, header.ts, header.caplen);
 		//dump_UDP_packet(packet, header.ts, header.caplen);
 		//hexDump("my_str", &header, sizeof(header));
-		/*dumpHex(packet, sizeof(packet) + 150);
-		printf("udp packet number: %d\nsizeof(data): %d\nsizeof(header): %d\n", counter, sizeof(data), sizeof(packet));
-		counter++;*/
-	//}
+		dumpHex(packet, sizeof(packet) + 150);
+		counter++;
+	}
 	//ipDump(pcap, sizeof(pcap) + 7500);
 
-	pcap_loop(pcap, 0, packet_handler, NULL);
+	pcap_close(pcap);
+
+	pcap = pcap_open_offline(filename, errbuf);
+	if (pcap == NULL) {
+		fprintf(stderr, "error reading pcap file: %s\n", errbuf);
+		exit(1);
+	}
+	int counter2 = 0;
+	pcap_loop(pcap, 0, packet_handler, NULL, counter2++);
 
 
 
